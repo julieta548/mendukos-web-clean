263466ae5a4bdce91eda7ba06e89db3f8b6605a8
Julieta Sosa <julietasosa548@gmail.com>
2025-10-04 19:21:25 -0300

    events mejorado

--- app/event-colectividades/page.tsx
+++ app/event-colectividades/page.tsx
@@ -1,4 +1,4 @@
-import EventMundial from "../../components/EventColectividades";
+import EventColectividades from "../components/EventColectividades";
 
 export default function Page() {
   return <EventColectividades />;
